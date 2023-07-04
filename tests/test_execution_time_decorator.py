716b19fe04c69ade3094b4c13c8c08f45c97a50a
Farzali Absalamov <fabsalamov@samhammer.de>
2023-07-04 16:15:47 +0200

    KIT-2621 updated unittests

--- tests/test_execution_time_decorator.py
+++ tests/test_execution_time_decorator.py
@@ -4,7 +4,13 @@ from typing import Any, cast
 from unittest import TestCase
 
 from sag_py_execution_time_decorator.execution_time_decorator import _calculate_and_log_execution_time
-from tests.test_data import SLEEP_TIME_MS, decorated_async_method, decorated_sync_method
+from tests.test_data import (
+    SLEEP_TIME_MS,
+    decorated_async_method,
+    decorated_async_method_extra_params,
+    decorated_sync_method,
+    decorated_sync_method_extra_params,
+)
 
 
 class PredictionControllerTest(TestCase):
@@ -34,6 +40,7 @@ class PredictionControllerTest(TestCase):
             self.assertEqual(log_record.function_name, "decorated_async_method")
             self.assertEqual(log_record.levelname, "ERROR")
             self.assertEqual(actual, "test: input")
+            self.assertTrue("param" not in log_record.__dict__.keys())
 
     def test__calculate_and_log_execution_time(self) -> None:
         # Act
@@ -46,3 +53,34 @@ class PredictionControllerTest(TestCase):
             self.assertEqual(log_record.name, "my_logger")
             self.assertEqual(log_record.levelname, "INFO")
             self.assertEqual(log_record.function_name, "my_func_name")
+            self.assertTrue("param" not in log_record.__dict__.keys())
+
+    def test_log_execution_time_sync_extra_params_function(self) -> None:
+        # Act
+        with self.assertLogs() as log_watcher:
+            actual = decorated_sync_method_extra_params("input")
+
+            # Assert
+            log_record = cast(Any, log_watcher.records[0])
+            self.assertAlmostEqual(log_record.execution_time, SLEEP_TIME_MS, delta=100)
+            self.assertEqual(log_record.function_name, "decorated_sync_method_extra_params")
+            self.assertEqual(log_record.levelname, "INFO")
+            self.assertDictContainsSubset({"param": "input"}, log_record.__dict__)
+            self.assertTrue("foo" not in log_record.__dict__.keys())
+            self.assertEqual(actual, "test: input")
+
+    def test_log_execution_time_async_extra_params_function(self) -> None:
+        # Arrange
+        loop = asyncio.get_event_loop()
+
+        # Act
+        with self.assertLogs() as log_watcher:
+            actual = loop.run_until_complete(decorated_async_method_extra_params("input"))
+
+            # Assert
+            log_record = cast(Any, log_watcher.records[0])
+            self.assertAlmostEqual(log_record.execution_time, SLEEP_TIME_MS, delta=100)
+            self.assertEqual(log_record.function_name, "decorated_async_method_extra_params")
+            self.assertEqual(log_record.levelname, "INFO")
+            self.assertDictContainsSubset({"param": "input"}, log_record.__dict__)
+            self.assertEqual(actual, "test: input")
